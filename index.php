7a6c13aa5366bddb2101bb4ab037d5bef74e7504
Scott thompson <Sthomspon42@mail.greenriver.edu>
2020-05-20 15:39:49 -0400

    survey page is working

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 session_start();
 require_once('vendor/autoload.php');
+require_once('models/data.php');
 
 //Instantiate the F3 Base class
 $f3 = Base::instance();
@@ -12,6 +13,20 @@ $f3->route('GET / ', function ($f3) {
 
     $view = new Template();
     echo $view->render('views/home.html');
+});
+
+$f3->route('GET|POST /survey ', function ($f3) {
+    $questions = getMidtermQuestions();
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $_SESSION['questions'] = $_POST['questions'];
+        $f3->reroute('summary');
+    }
+
+    $f3->set('questions', $questions);
+
+    $view = new Template();
+    echo $view->render('views/survey.html');
 });
 
 $f3-> run();
---
+++ models/data.php
@@ -0,0 +1,6 @@
+<?php
+
+function getMidtermQuestions(){
+    return array("This Midterm is easy", "I like Midterms", "Today is Monday");
+}
+
